f5f392ed8946c12875a37fef94c5e7603b78e4c3
theanmolsharma <anmolsharma0234@gmail.com>
2023-12-17 17:15:27 +0530

    feat: added support to connect to database
    Signed-off-by: theanmolsharma <anmolsharma0234@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,28 @@ import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import configuration from '@app/configuration';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             ignoreEnvFile: true,
             load: [configuration],
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get<string>('db.postgres.host'),
+                port: configService.get<number>('db.postgres.port'),
+                username: configService.get<string>('db.postgres.username'),
+                password: configService.get<string>('db.postgres.password'),
+                database: configService.get<string>('db.postgres.databaseName'),
+                synchronize: configService.get<boolean>('db.synchronize'),
+                autoLoadEntities: true,
+            }),
         }),
     ],
     controllers: [AppController],
