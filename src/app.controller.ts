af9178c0662e25336f7c99833be3ca28448c71e5
theanmolsharma <anmolsharma0234@gmail.com>
2023-12-17 15:42:55 +0530

    chore: added swagger to auto-generate API spec
    Signed-off-by: theanmolsharma <anmolsharma0234@gmail.com>

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
 import { AppService } from '@app/app.service';
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
+    @ApiTags('Service Health')
+    @ApiOperation({
+        description: 'Check health of app',
+    })
     @Get('/health')
     getHealth(): string {
         return this.appService.getHealth();
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { AppModule } from '@app/app.module';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Worksat API')
+        .setDescription('Backend services for Worksats.')
+        .setVersion('0.0.1')
+        .build();
+    const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, swaggerDocument);
+
     await app.listen(3000);
 }
 bootstrap();
